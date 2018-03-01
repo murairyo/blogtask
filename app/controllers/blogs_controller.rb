44fd7a89b4f4871b18f9b22cf7d4a748272e1c4e
murai1027 <rzdb3693@gmail.com>
2018-03-01 10:51:48 +0000

    完了

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,23 +1,59 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update,:destroy]
   def index
     @blogs = Blog.all
+    # binding.pry
   end
   
   def new
-    @blog = Blog.new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
+      @blog = Blog.new
+    end
+  end
+  
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
   end
   
   def create
-     Blog.create(blog_params)
-     redirect_to new_blog_path
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render 'new'
+    end
   end
   
   def show
-    @blog = Blog.find(params[:id])
+   
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
   end
   
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
